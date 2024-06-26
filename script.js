dad333ced36b5914b6ad1b7e1a1d016be336336f
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-26 16:29:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+function findDomElement(elementId) {
+	let element  = document.getElementById('elementId')
+	let level = 0
+
+	while (element) {
+		level++;
+		element = element.parentElement;
+	}
+	return level
+}
+
+let domLevel = findDomElement('level')
+
+alert('The level of the element is: ' + domLevel);
