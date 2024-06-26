cb29c2fd0fbf601bd531b8cc38c1c0b5d23c0068
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-26 17:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 
 function findDomElement(elementId) {
-	let element  = document.getElementById('elementId')
+	let element  = document.getElementById(elementId)
 	let level = 0
 
 	while (element) {
